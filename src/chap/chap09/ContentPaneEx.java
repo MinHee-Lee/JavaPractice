2b48b6786232f1f089ad7418e19cbd3d1c117734
MinHee-Lee <57509357+MinHee-Lee@users.noreply.github.com>
2021-11-10 10:28:01 +0900

    Chap09

---
+++ src/chap/chap09/ContentPaneEx.java
@@ -0,0 +1,28 @@
+package chap.chap09;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPaneEx extends JFrame {
+    public ContentPaneEx(){
+        setTitle("ContentPane과 JFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setBackground(Color.orange);
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("OK"));
+        contentPane.add(new JButton("Cancle"));
+        contentPane.add(new JButton("Ignore"));
+
+        setSize(300,150);
+        setVisible(true);
+
+
+    }
+
+    public static void main(String[] args) {
+        new ContentPaneEx();
+    }
+}
