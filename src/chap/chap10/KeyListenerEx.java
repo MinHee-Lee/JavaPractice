cf4212e758f28e82d0798475792b6e6c2fb5237f
MinHee-Lee <57509357+MinHee-Lee@users.noreply.github.com>
2021-11-19 08:40:15 +0900

    Chap10

---
+++ src/chap/chap10/KeyListenerEx.java
@@ -0,0 +1,51 @@
+package chap.chap10;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class KeyListenerEx extends JFrame {
+    private JLabel [] keyMessage;
+
+    public KeyListenerEx(){
+        setTitle("keyListener 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+
+        c.addKeyListener(new MyKeyListener());
+
+        keyMessage = new JLabel[3];
+        keyMessage[0] = new JLabel(" getKeyCode() ");
+        keyMessage[1] = new JLabel(" getKeyChar() ");
+        keyMessage[2] = new JLabel(" getKeyText() ");
+
+        for (int i = 0; i<keyMessage.length; i++){
+            c.add(keyMessage[i]);
+            keyMessage[i].setOpaque(true);
+            keyMessage[i].setBackground(Color.YELLOW);
+        }
+
+        setSize(350,150);
+        setVisible(true);
+
+        c.setFocusable(true);
+        c.requestFocus();
+    }
+
+    class MyKeyListener extends KeyAdapter{
+        public void keyPresses(KeyEvent e){
+            int keyCode = e.getKeyCode();
+            char ketChar = e.getKeyChar();
+
+            keyMessage[0].setText(Integer.toString(keyCode));
+            keyMessage[1].setText(Character.toString(ketChar));
+            keyMessage[2].setText(e.getKeyText(keyCode));
+        }
+    }
+
+    public static void main(String[] args) {
+        new KeyListenerEx();
+    }
+
+}
